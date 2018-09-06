79a4303be65ea6e9b5310292f490d440a0b97d7f
Juan Manuel Mougan <juanmougan@gmail.com>
2018-09-06 20:49:03 -0300

    Awful version that just works ®

--- src/main/java/com/github/juanmougan/zip/Zipper.java
+++ src/main/java/com/github/juanmougan/zip/Zipper.java
@@ -1,8 +1,18 @@
 package com.github.juanmougan.zip;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class Zipper {
@@ -13,5 +23,34 @@ public class Zipper {
         } catch (IOException e) {
             throw new RuntimeException("Error opening zip file", e);
         }
+    }
+
+    public void unzipFilesOnly(PrintStream stream, Path zipPath) {
+        Predicate<ZipEntry> isFile = ze -> !ze.isDirectory();
+        try(ZipFile zipFile = new ZipFile(zipPath.toFile())) {
+            List<? extends ZipEntry> entries = zipFile.stream().filter(isFile).collect(Collectors.toList());
+            // Nasty hack to keep names
+            List<String> names = new ArrayList<>();
+            List<InputStream> inputStreams = entries.stream().map(ze -> {
+                try {
+                    names.add(ze.getName());
+                    return zipFile.getInputStream(ze);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }).collect(Collectors.toList());
+            // I'm not using a Stream here, because I want an index --due to upper's another awful hack
+            for (int i = 0; i < inputStreams.size(); i++) {
+                 // Copy each stream to a new file
+                Files.copy(inputStreams.get(i), resolveAbsolutePath(zipPath, names.get(i) + new Date()));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error opening zip file", e);
+        }
+    }
+
+    private Path resolveAbsolutePath(Path zipPath, String fileName) {
+        return zipPath.getParent().toAbsolutePath().resolve(fileName);
     }
 }
--- src/test/java/com/github/juanmougan/zip/ZipperIntegrationTest.java
+++ src/test/java/com/github/juanmougan/zip/ZipperIntegrationTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,21 +15,30 @@ class ZipperIntegrationTest {
 
     private static final String ZIP_FILE = "twoFiles.zip";
     private Zipper zipper;
+    private URL zipUrl;
 
     @BeforeEach
     void setUp() {
         this.zipper = new Zipper();
+        this.zipUrl = this.getClass().getResource("/" + ZIP_FILE);
     }
 
     @AfterEach
     void tearDown() {
         this.zipper = null;
+        this.zipUrl = null;
     }
 
     @Test
     void printEntries() {
-        System.out.println("Absolute: " + this.getClass().getResource("/" + ZIP_FILE));
-        System.out.println("As stream: " + this.getClass().getResourceAsStream("/" + ZIP_FILE));
-        this.zipper.printEntries(System.out, this.getClass().getResource("/" + ZIP_FILE));
+        this.zipper.printEntries(System.out, zipUrl);
+    }
+
+    @Test
+    void unzipFilesOnly() throws URISyntaxException {
+        zipUrl = this.getClass().getResource("/" + ZIP_FILE);
+        System.out.println(Paths.get(zipUrl.toURI()).getParent().toAbsolutePath());
+        this.zipper.unzipFilesOnly(System.out, Paths.get(zipUrl.toURI()));
+        System.out.println("Finished");
     }
 }
