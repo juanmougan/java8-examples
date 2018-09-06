07a7cdb9a3681d0732f219a56a97a08d9822f20d
Juan Manuel Mougan <juanmougan@gmail.com>
2018-09-06 20:52:51 -0300

    Added useful links

--- src/main/java/com/github/juanmougan/zip/Zipper.java
+++ src/main/java/com/github/juanmougan/zip/Zipper.java
@@ -9,12 +9,19 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+/**
+ * Zip implementation using Java's APIs
+ * Big thanks to: <ul>
+ *          <li>http://blog.codeleak.pl/2014/06/listing-zip-file-content-java-8.html<li/>
+ *          <li>https://stackoverflow.com/a/36890475/3923525<li/>
+ *     <ul/>
+ *     for the inspiration :)
+ */
 public class Zipper {
     public void printEntries(PrintStream stream, URL zip) {
         try (ZipFile zipFile = new ZipFile(zip.getFile())) {
@@ -27,7 +34,7 @@ public class Zipper {
 
     public void unzipFilesOnly(PrintStream stream, Path zipPath) {
         Predicate<ZipEntry> isFile = ze -> !ze.isDirectory();
-        try(ZipFile zipFile = new ZipFile(zipPath.toFile())) {
+        try (ZipFile zipFile = new ZipFile(zipPath.toFile())) {
             List<? extends ZipEntry> entries = zipFile.stream().filter(isFile).collect(Collectors.toList());
             // Nasty hack to keep names
             List<String> names = new ArrayList<>();
@@ -42,7 +49,7 @@ public class Zipper {
             }).collect(Collectors.toList());
             // I'm not using a Stream here, because I want an index --due to upper's another awful hack
             for (int i = 0; i < inputStreams.size(); i++) {
-                 // Copy each stream to a new file
+                // Copy each stream to a new file
                 Files.copy(inputStreams.get(i), resolveAbsolutePath(zipPath, names.get(i) + new Date()));
             }
         } catch (IOException e) {
